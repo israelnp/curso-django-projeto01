d067a418e18ad813c0c204a9ec2bdd1cf6aca8cd
israelnp <israelpereira1984@gmail.com>
2023-01-05 18:55:43 -0300

    add urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from utils.recipes.factory import make_recipe
 
 
@@ -11,4 +12,5 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
