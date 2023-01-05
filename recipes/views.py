3534d6b5c448da766940cdc5064d47a69c982b87
israelnp <israelpereira1984@gmail.com>
2023-01-05 18:08:43 -0300

    inject data list

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/page/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/page/recipe_view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
